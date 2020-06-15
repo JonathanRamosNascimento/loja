2dcc2248a22c3cda8c1ac45f7277a9ede2dd4bc5
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-06-15 20:54:14 -0300

    Criando operação de instanciação de objetos para teste inicial

--- src/main/java/com/jonathan/loja/LojaApplication.java
+++ src/main/java/com/jonathan/loja/LojaApplication.java
@@ -1,13 +1,31 @@
 package com.jonathan.loja;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jonathan.loja.domain.Categoria;
+import com.jonathan.loja.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class LojaApplication {
+public class LojaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
