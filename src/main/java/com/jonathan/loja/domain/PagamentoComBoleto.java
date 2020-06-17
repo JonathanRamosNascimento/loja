107ca736f8b287e7b3a59899a8ec1422ca243a55
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-06-16 22:52:39 -0300

    Criando as entidades de pagamento, pedido, estado... e relacionamento bidirecional OneToOne de pedido e pagamento onde usa o Id de pedido no Id de pagamento

---
+++ src/main/java/com/jonathan/loja/domain/PagamentoComBoleto.java
@@ -0,0 +1,41 @@
+package com.jonathan.loja.domain;
+
+import java.util.Date;
+
+import com.jonathan.loja.domain.enums.EstadoPagamento;
+
+public class PagamentoComBoleto extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	
+	private Date dataVencimento;
+	private Date dataPagamento;
+	
+	public PagamentoComBoleto() {
+		
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
+		super(id, estado, pedido);
+		this.dataPagamento = dataPagamento;
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+	
+	
+
+}
