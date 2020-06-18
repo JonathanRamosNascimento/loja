38306fca7f9df9cd66d7406b20d2ebe1d7954fac
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-06-18 08:36:37 -0300

    Endpoint /pedidos/{id} finalizado e com suas devidas retrições para gerar o json

---
+++ src/main/java/com/jonathan/loja/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.jonathan.loja.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jonathan.loja.domain.Pedido;
+import com.jonathan.loja.repositories.PedidoRepository;
+import com.jonathan.loja.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
