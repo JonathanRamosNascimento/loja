73867771b70e4b2bb7cf9d07b5a4d77a49bc5158
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-06-19 08:30:19 -0300

    Deletando uma categoria e fazendo os tratamentos de excessão para o método

--- src/main/java/com/jonathan/loja/services/CategoriaService.java
+++ src/main/java/com/jonathan/loja/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.jonathan.loja.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.jonathan.loja.domain.Categoria;
 import com.jonathan.loja.repositories.CategoriaRepository;
+import com.jonathan.loja.services.exceptions.DataIntegrityException;
 import com.jonathan.loja.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,4 +33,12 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
+	}
 }
