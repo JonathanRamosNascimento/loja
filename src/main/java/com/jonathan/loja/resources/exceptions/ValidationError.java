8c0e37e873b13bd02237714521690043c719fe41
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-06-22 08:41:16 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/jonathan/loja/resources/exceptions/ValidationError.java
@@ -0,0 +1,23 @@
+package com.jonathan.loja.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErros() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+}
