9107c8385acc0de06599b937fb000cc2729b16ad
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-07-01 09:13:10 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/jonathan/loja/repositories/ProdutoRepository.java
+++ src/main/java/com/jonathan/loja/repositories/ProdutoRepository.java
@@ -1,11 +1,23 @@
 package com.jonathan.loja.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import com.jonathan.loja.domain.Categoria;
 import com.jonathan.loja.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+  // @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat
+  // WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+  @Transactional(readOnly = true)
+  Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias,
+      Pageable pageRequest);
 
 }
