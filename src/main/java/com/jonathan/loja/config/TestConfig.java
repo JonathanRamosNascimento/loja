25ceb8b53123b60fb748c6b806ce132b430a55b5
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-07-16 08:34:38 -0300

    Criando profile de test

---
+++ src/main/java/com/jonathan/loja/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.jonathan.loja.config;
+
+import java.text.ParseException;
+
+import com.jonathan.loja.services.DBService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+  @Autowired
+  private DBService dbService;
+
+  @Bean
+  public boolean instantiateDatabse() throws ParseException {
+    dbService.instantiateTestDatabase();
+    return true;
+  }
+}
